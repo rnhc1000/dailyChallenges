d11fb50eccfbb02df5294e370290b556ce817b2b
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-04-05 23:49:51 -0300

    findElement com uso de map e filter

--- typeScript/findElement.js
+++ typeScript/findElement.js
@@ -1,13 +1,39 @@
-function findElement(arr, func) {
-    let num = 0;
+/**
+ * 
+ * @param {*} arr 
+ * @param {*} func 
+ * @returns 
+ */
+
+function findElement(arr, findOdd) {
+  let pos = [];
+    let num = [];
     for (let idx in arr) {
-      num = func(arr[idx]);
-      if (num) {
-        return arr[idx];
+      num[idx] = findOdd(arr[idx]);
+      if (num[idx]) {
+
+        pos[idx] = idx
+
       }
-    } return undefined;
+      console.log(num);
+      // if (num) {
+      //   return arr[idx];
+      // }
+    } //return undefined;
+    console.log(pos);
+    const position = pos.filter(pos => pos !== "");
+    console.log(position);
+    const resp = num.filter(num => num === true);
+    console.log(resp)
+
   }
 
-  let x = findElement([1, 2, 3, 5], num => num % 2 === 1);
+
+  function findOdd(num) {
+
+    return num % 2 == 1;
+  }
+
+  let x = findElement([1, 2, 3, 5], findOdd);
 
   console.log(x);
